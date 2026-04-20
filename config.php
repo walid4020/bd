37cb298828d9992a5bee1dcc9a2036fbedb238b2
walidlouh <walid.louh@student.hepl.be>
2026-04-20 16:13:08 +0200

    lien php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+define ('USER,'vy44dy72oodv');
+define ('PASSWD','d3-d2d!4oo');
+define ( 'SERVER','localhost');
+define('BASE','ebus2_projet04_viiy78'); 
+
+$dsn = 'mysql:host' . SERVER . '; dbname=' . BASE;
+
+try {
+$connexion = new PDO($dsn, USER, PASSWD);
+}catch(PDOException $e) {
+  echo 'Échec de la connexion : ' . $e->getMessage();
+  exit();
+}
+
