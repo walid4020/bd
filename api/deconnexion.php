1941d12ec8071a6d302153ba2d13c043d6cb3ab0
Noémie <noemie.asselborn@student.hepl>
2026-05-12 09:04:50 +0200

    Ajout d'un fichier deconnexion.php

---
+++ api/deconnexion.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Supprime toutes les données de la session en cours
+session_unset();
+
+// Détruit la session côté serveur
+session_destroy();
+
+// Redirige vers la page de connexion
+header('Location: ../index.html');
+exit;
+?>
