1095891d17c07ead588cd934ee16493239c63e20
Noémie <noemie.asselborn@student.hepl>
2026-05-13 09:05:36 +0200

    modif bug doublon

--- app.js
+++ app.js
@@ -10,21 +10,9 @@ createApp({
             // Stocke les messages d'erreur à afficher dans le formulaire
             error: '',
             // Contrôle l'ouverture/fermeture du menu hamburger sur mobile
-                menuOuvert: false,
-                Ce qui donne :
-
-                data() {
-                    return {
-                        currentUser: null,
-                        error: '',
-                        // Contrôle l'ouverture/fermeture du menu hamburger sur mobile
-                        menuOuvert: false,
-                        login_form: ref({ email: '', password: '', remember: false })
-                    }
-                },
-            // Les données du formulaire de connexion
-            // On utilise "email" pour correspondre au v-model HTML
-            login_form: ref ({ email: '', password: '', remember: false })
+            menuOuvert: false,
+            // Données du formulaire de connexion
+            login_form: ref({ email: '', password: '', remember: false })
         }
     },
 
