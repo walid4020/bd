573b3b0e51507218dafecabce4a21c33ff4c9d86
Noémie <noemie.asselborn@student.hepl>
2026-05-13 21:52:56 +0200

    modif taille formulaire sur mobile

--- api/formulaire_modification_depense.php
+++ api/formulaire_modification_depense.php
@@ -61,7 +61,7 @@ $groupe = $stmt_groupe->fetch(PDO::FETCH_ASSOC);
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title> Modifier une dépense </title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
     </head>
